7b05e8c7d8e142e8fb3d192c7e885073dc123d21
kyy1204 <rezky.ochan@gmail.com>
2025-06-12 12:24:15 +0800

    membuat mutator

--- Rumus.java
+++ Rumus.java
@@ -1,29 +1,32 @@
-package Latihan3;
-
+package latihan3;
+  //Nama : Muhammad Rezkyan Noor, NPM : 2310010087, Kelas : 4c
 public class Rumus {
     int sisi;
     int panjang;
     int lebar;
     int hasil;
-    
-    
-        public Rumus() {
-        sisi = 0;
-        panjang = 0;
-        lebar = 0;
-        hasil = 0;
-        }
-        
-    // Method menghitung luas persegi
+
     public void hitungLuasPersegi() {
         hasil = sisi * sisi;
-        System.out.println("Luas Persegi = " + hasil);
+        
+        System.out.println("Luas Persegi =" +hasil);
+    }
+     public void hitungLuasPersegiPanjang() {
+        hasil = panjang * lebar;
+        
+        System.out.println("Luas Persegi Panjang =" +hasil);
+     }
+
+    public int getHasil() {
+        return hasil;
     }
 
-    // Method menghitung luas persegi panjang
-    public void hitungLuasPersegiPanjang() {
-        hasil = panjang * lebar;
-        System.out.println("Luas Persegi Panjang = " + hasil);
-    }
-
+    public Rumus(int sisi, int panjang, int lebar, int hasil) {
+        this.sisi = sisi;
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.hasil = hasil;
+    }
+     
+     
 }
